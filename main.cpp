58aabc2e5fdb09c4c077b889fe9b3bfec7c9dd9e
JiauZhang <1743960454@qq.com>
2023-08-17 07:24:39 +0800

    support cmdline args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (C) 2023 JiauZhang <https://github.com/JiauZhang/menuconfig>
 
+extern "C" {
 #include <signal.h>
 #include <string.h>
 #include "lkc.h"
@@ -14,33 +15,60 @@ extern int handle_exit_api(void);
 
 extern int *silent_api;
 extern int *single_menu_mode_api;
+}
 
-int main(int ac, char **av)
+#include "argparse.hpp"
+
+int main(int argc, char **argv)
 {
     char *mode;
     int res;
+    argparse::ArgumentParser parser("menuconfig options");
+
+    parser.add_argument("-s")
+        .help("change output verbosity")
+        .default_value(false)
+        .implicit_value(true);
+    parser.add_argument("--kconfig")
+        .help("specify the Kconfig file");
+
+    std::cout << "parse args...\n";
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cout << "runtime error:\n";
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
+    std::cout << "parse args done!\n";
 
     signal(SIGINT, sig_handler_api);
 
-    if (ac > 1 && strcmp(av[1], "-s") == 0)
-    {
+    if (parser["-s"] == true) {
         *silent_api = 1;
         /* Silence conf_read() until the real callback is set up */
         conf_set_message_callback(NULL);
-        av++;
     }
-    conf_parse(av[1]);
+
+    std::string kconfig("./Kconfig");
+
+    std::cout << "name_cmdline" << "\n";
+    if (parser.is_used("--kconfig")) {
+        kconfig = parser.get<std::string>("--kconfig");
+        std::cout << kconfig << "\n";
+    }
+
+    conf_parse(kconfig.c_str());
     conf_read(NULL);
 
     mode = getenv("MENUCONFIG_MODE");
-    if (mode)
-    {
+    if (mode) {
         if (!strcasecmp(mode, "single_menu"))
             *single_menu_mode_api = 1;
     }
 
-    if (init_dialog(NULL))
-    {
+    if (init_dialog(NULL)) {
         fprintf(stderr, "Your display is too small to run Menuconfig!\n");
         fprintf(stderr, "It must be at least 19 lines by 80 columns.\n");
         return 1;
@@ -48,8 +76,7 @@ int main(int ac, char **av)
 
     set_config_filename_api(conf_get_configname());
     conf_set_message_callback(conf_message_callback_api);
-    do
-    {
+    do {
         conf_api(&rootmenu, NULL);
         res = handle_exit_api();
     } while (res == KEY_ESC);
